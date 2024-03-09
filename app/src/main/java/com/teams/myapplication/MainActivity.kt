e8a5d35683d46f269c6a5a171f869545f5eb8405
Saransh Nirmalkar <saransh0111@gmail.com>
2024-03-09 11:23:20 +0530

    comit 1

--- app/src/main/java/com/teams/myapplication/MainActivity.kt
+++ app/src/main/java/com/teams/myapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.teams.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.teams.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        start()
     }
+
+    private fun start(){
+
+    }
+
+    private fun letsDo(){
+
+    }
+
 }
